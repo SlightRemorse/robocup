38bb785e79eb6796cd22ebd86d5e21791638d81e
Boris Yonchev <b.yonchev@robopartans.com>
2012-10-31 19:39:49 +0200

    Moved the legacy code to a separate folder and added the b.yonchev and d.vlahovski remote control code

---
+++ legacy/2011/byonchev_dvlahovski/control/remote_control/RemoteControl_Robot/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace RemoteControl_Robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort device = new SerialPort("COM6", 9600);
+
+            device.Open();
+
+            while (true)
+            {
+                int command = 0;
+
+                try
+                {
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.LeftArrow:
+						{
+							command = 1;
+							break;
+						}
+
+                        case ConsoleKey.RightArrow:
+						{
+							command = 2;
+
+							break;
+						}
+
+                        case ConsoleKey.UpArrow:
+						{
+							command = 3;
+
+							break;
+						}
+
+                        case ConsoleKey.DownArrow:
+						{
+							command = 4;
+
+							break;
+						}
+
+                        default: // Any key pressed -> Stop
+						{
+							command = 0;
+							break;
+
+						}
+                    }
+                }
+                catch (Exception ex)
+                {
+                    break;
+                }
+
+                char c = (char)command;
+                string toSend = new string(c, 1);
+
+                device.Write(toSend);
+
+            }
+
+            device.Close();
+        }
+    }
+}
